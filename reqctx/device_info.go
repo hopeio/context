b02e3907c59d6954391a57b4e134aedac995bffe
贾一饼 <lby.i@qq.com>
2024-07-29 18:55:18 +0800

    fix: location type

--- reqctx/device_info.go
+++ reqctx/device_info.go
@@ -1,18 +1,21 @@
 package reqctx
 
-import "net/url"
+import (
+	"net/url"
+	"strconv"
+)
 
 type DeviceInfo struct {
 	//设备
-	Device     string `json:"device" gorm:"size:255"`
-	OS         string `json:"os" gorm:"size:255"`
-	AppCode    string `json:"appCode" gorm:"size:255"`
-	AppVersion string `json:"appVersion" gorm:"size:255"`
-	IP         string `json:"ip" gorm:"size:255"`
-	Lng        string `json:"lng" gorm:"type:numeric(10,6)"`
-	Lat        string `json:"lat" gorm:"type:numeric(10,6)"`
-	Area       string `json:"area" gorm:"size:255"`
-	UserAgent  string `json:"userAgent" gorm:"size:255"`
+	Device    string  `json:"device" gorm:"size:255"`
+	OS        string  `json:"os" gorm:"size:255"`
+	AppCode   string  `json:"appCode" gorm:"size:255"`
+	AppVer    string  `json:"appVer" gorm:"size:255"`
+	IP        string  `json:"ip" gorm:"size:255"`
+	Lng       float64 `json:"lng" gorm:"type:numeric(10,6)"`
+	Lat       float64 `json:"lat" gorm:"type:numeric(10,6)"`
+	Area      string  `json:"area" gorm:"size:255"`
+	UserAgent string  `json:"userAgent" gorm:"size:255"`
 }
 
 func Device(infoHeader, area, localHeader, userAgent, ip string) *DeviceInfo {
@@ -23,7 +26,7 @@ func Device(infoHeader, area, localHeader, userAgent, ip string) *DeviceInfo {
 		unknow = false
 		var n, m int
 		for i, c := range infoHeader {
-			if c == '-' {
+			if c == ',' {
 				switch n {
 				case 0:
 					info.Device = infoHeader[m:i]
@@ -32,7 +35,7 @@ func Device(infoHeader, area, localHeader, userAgent, ip string) *DeviceInfo {
 				case 2:
 					info.AppCode = infoHeader[m:i]
 				case 3:
-					info.AppVersion = infoHeader[m:i]
+					info.AppVer = infoHeader[m:i]
 				}
 				m = i + 1
 				n++
@@ -49,12 +52,12 @@ func Device(infoHeader, area, localHeader, userAgent, ip string) *DeviceInfo {
 		unknow = false
 		var n, m int
 		for i, c := range localHeader {
-			if c == '-' {
+			if c == ',' {
 				switch n {
 				case 0:
-					info.Lng = localHeader[m:i]
+					info.Lng, _ = strconv.ParseFloat(localHeader[m:i], 64)
 				case 1:
-					info.Lat = localHeader[m:i]
+					info.Lat, _ = strconv.ParseFloat(localHeader[m:i], 64)
 				}
 				m = i + 1
 				n++
